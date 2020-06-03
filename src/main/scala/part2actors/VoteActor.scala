8fd6c22ea6a67e3dc2a4638e01e27758f3979144
dinukadev <dinuka.arseculeratne@gmail.com>
2020-06-03 16:38:53 +1000

    voting aggregator

---
+++ src/main/scala/part2actors/VoteActor.scala
@@ -0,0 +1,62 @@
+package part2actors
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+
+object VoteActor extends App {
+
+  case class Vote(candidate: String)
+
+  case object VoteStatusRequest
+
+  case class VoteStatusReply(candidate: Option[String])
+
+  class Citizen extends Actor {
+    var candidate: Option[String] = None
+
+    override def receive: Receive = {
+      case Vote(c) => this.candidate = Some(c)
+      case VoteStatusRequest => sender() ! VoteStatusReply(candidate)
+    }
+  }
+
+  case class AggregateVotes(citizens: Set[ActorRef])
+
+  class VoteAggregator extends Actor {
+    var stillWaiting: Set[ActorRef] = Set()
+    var currentStats: Map[String, Int] = Map()
+
+    override def receive: Receive = {
+      case AggregateVotes(citizens) =>
+        stillWaiting = citizens
+        citizens.foreach(citizenRef => citizenRef ! VoteStatusRequest)
+      case VoteStatusReply(None) =>
+        sender() ! VoteStatusRequest
+
+      case VoteStatusReply(Some(candidate)) =>
+        val newStillWaiting = stillWaiting - sender()
+        val currentVotesOfCandidate = currentStats.getOrElse(candidate, 0)
+        currentStats = currentStats + (candidate -> (currentVotesOfCandidate + 1))
+        if (newStillWaiting.isEmpty) {
+          println(s"[aggregator] poll stats: $currentStats")
+        } else {
+          stillWaiting = newStillWaiting
+        }
+    }
+  }
+
+  val system = ActorSystem("votingSystem")
+  val alice = system.actorOf(Props[Citizen])
+  val bob = system.actorOf(Props[Citizen])
+  val charlie = system.actorOf(Props[Citizen])
+  val daniel = system.actorOf(Props[Citizen])
+
+  alice ! Vote("Martin")
+  bob ! Vote("Jonas")
+  charlie ! Vote("Roland")
+  daniel ! Vote("Roland")
+
+  val voteAggregator = system.actorOf(Props[VoteAggregator])
+  voteAggregator ! AggregateVotes(Set(alice, bob, charlie, daniel))
+
+
+}
