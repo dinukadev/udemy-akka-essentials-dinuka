c94a9c7442c2bdb0686c14e6857bcf854b4d77ce
dinukadev <dinuka.arseculeratne@gmail.com>
2020-06-09 21:54:58 +1000

    intercept log messages

---
+++ src/main/scala/part03testing/InterceptingLogSpec.scala
@@ -0,0 +1,89 @@
+package part03testing
+
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import akka.testkit.{EventFilter, ImplicitSender, TestKit}
+import com.typesafe.config.ConfigFactory
+import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
+import part03testing.InterceptingLogSpec.{Checkout, CheckoutActor}
+
+class InterceptingLogSpec extends TestKit(ActorSystem("InterceptingLogSpec", ConfigFactory.load().getConfig("interceptingLogMessages")))
+  with ImplicitSender
+  with WordSpecLike
+  with BeforeAndAfterAll {
+
+  override protected def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  val item = "Rock the JVM Akka course"
+  val creditCard = "111-111-11-11"
+  "A checkout flow" should {
+    "correctly log the dispath of an order" in {
+      EventFilter.info(pattern = s"Order [0-9]+ for item $item has been dispatched.", occurrences = 1) intercept {
+        val checkoutRef = system.actorOf(Props[CheckoutActor])
+        checkoutRef ! Checkout(item, creditCard)
+      }
+    }
+  }
+
+}
+
+object InterceptingLogSpec {
+
+  case class Checkout(item: String, creditCard: String)
+
+  case class AuthorizeCard(creditCard: String)
+
+  case object PaymentAccepted
+
+  case object PaymentDenied
+
+  case class DispathOrder(item: String)
+
+  case object OrderConfirmed
+
+
+  class CheckoutActor extends Actor {
+    private val paymentManager = context.actorOf(Props[PaymentManager])
+    private val fullfillmentManager = context.actorOf(Props[FullfillmentManager])
+
+    override def receive: Receive = awaitingCheckout
+
+    def awaitingCheckout: Receive = {
+      case Checkout(item, card) =>
+        paymentManager ! AuthorizeCard(card)
+        context.become(pendingPayment(item))
+    }
+
+    def pendingPayment(item: String): Receive = {
+      case PaymentAccepted =>
+        fullfillmentManager ! DispathOrder(item)
+        context.become(pendingFullfillment(item))
+    }
+
+    def pendingFullfillment(item: String): Receive = {
+      case OrderConfirmed => context.become(awaitingCheckout)
+    }
+  }
+
+  class PaymentManager extends Actor {
+    override def receive: Receive = {
+      case AuthorizeCard(card) =>
+        if (card.startsWith("0")) sender() ! PaymentDenied
+        else sender() ! PaymentAccepted
+    }
+  }
+
+  class FullfillmentManager extends Actor with ActorLogging {
+    var orderId = 0
+
+    override def receive: Receive = {
+      case DispathOrder(item) =>
+        orderId += 1
+        log.info(s"Order $orderId for item $item has been dispatched.")
+        sender() ! OrderConfirmed
+
+    }
+  }
+
+}
